811ff2064877a716d704ea4ccd8fefabb5968299
Axatao <axatao@proton.me>
2026-05-17 14:44:13 +0200

    feat:  crear cielo.

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,9 @@
-<script lang="ts">
-  let count = 0
-  const increment = () => (count += 1)
+<script>
+  import Sky from "./components/Sky.svelte";
 </script>
 
 <main>
-  <h1>Ejemplo mínimo</h1>
-  <button class="counter" on:click={increment}>Contador: {count}</button>
+  <section>
+    <Sky />
+  </section>
 </main>
---
+++ src/components/Sky.svelte
@@ -0,0 +1,1 @@
+<div class="w-full h-screen bg-gradient-to-b from-[#06062d] to-[#14184f]"></div>
